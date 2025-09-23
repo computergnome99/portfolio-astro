54db47b79a1a7cd54817b12d619ff042e9501bef
Calvin Bonner <me@calvinbonner.com>
2025-09-23 14:25:41 -0400

    Added robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,30 @@ import svelte from '@astrojs/svelte';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), svelte()],
+  site: 'https://calvinbonner.dev',
+  integrations: [
+    mdx(),
+    svelte(),
+    sitemap({
+      changefreq: 'weekly',
+      lastmod: new Date(Date.now()),
+      serialize(item) {
+        if (/experience\/./.test(item.url)) {
+          item.priority = 0.7;
+        } else {
+          item.priority = 1;
+        }
+
+        return item;
+      },
+    }),
+  ],
   markdown: {
     shikiConfig: {
       themes: {
